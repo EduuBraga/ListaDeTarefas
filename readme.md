02b58799cd12cfdec5f80a2a85afa2cbbb6fdf40
EduuBraga <chocolate.blindado123@gmail.com>
2022-07-12 22:34:03 -0300

    Finalizando algoritmo JS
    Finalizando algoritmo JS e adicionadno Readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Projeto Lista de Tarefas
+
+<p>Neste projeto utilizo o método localStorage para armazenar tarefas no seu serviço local, sendo assim mesmo que você atualize a página ou feche a mesma suas tarefas vão continuar do mesmo jeito.</p>
--- script.js
+++ script.js
@@ -1,38 +1,69 @@
-let textoTarefa = document.querySelector("input")
-let botao = document.querySelector("button")
-let listaDeTarefa = document.querySelector(".lista-de-tarefa")
-let lixeira = document.querySelector(".lixeira")
+// declarando variáveis
+const botaoNvTrf = document.querySelector(".botao")
+const listaDeTarefa = document.querySelector(".lista-de-tarefa")
+const botaoLpTrf = document.querySelector(".botao-limpar")
+const textoTrf = document.querySelector("input")
+const lixeira = document.querySelectorAll(".lixeira")
 
-let lista = []
+let tarefas = []
 
-// Recarregando tarefas.
+//Evento para carregar as tarefas
 onload = () => {
+    tarefas = JSON.parse(localStorage.getItem("tarefas"))
 
-    let lista = JSON.parse(localStorage.getItem("tarefas"))
+    if (tarefas == null) {
+        tarefas = []
+    }
 
-    lista.forEach(elemento => {
-        let conteudo = `<li class='item-lista'> <p class='tarefa'>${elemento}</p> <img class='lixeira' src='./images/trash-can.png' alt='lixeira'> </li>`
-        
+    tarefas.forEach(element => {
+        let conteudo = `<div  identificador="${element}" class="item-lista"><p>${element}</p><img onclick="excluirTarefa(this)" src="images/trash-can.png" class="lixeira" alt="lixeira" ></div>`
+
         listaDeTarefa.innerHTML += conteudo
-    })
+    });
 
 }
 
-// Adicionando tarefa
-botao.addEventListener('click', () => {
+//Evento para insirir uma nova tarefa
+botaoNvTrf.addEventListener('click', () => {
+    let tarefa = textoTrf.value
 
-    lista.push(textoTarefa.value)
-
-    let valor = JSON.stringify(lista)
-
-    let conteudo = `<li class='item-lista'> <p class='tarefa'>${textoTarefa.value}</p> <img class='lixeira' src='./images/trash-can.png' alt='lixeira'> </li>`
+    let conteudo = `<div identificador="${tarefa}" class="item-lista"><p>${tarefa}</p><img onclick="excluirTarefa(this)" src="images/trash-can.png" class="lixeira" alt="lixeira" ></div>`
 
     listaDeTarefa.innerHTML += conteudo
 
-    localStorage.setItem("tarefas", valor)
+    tarefas.push(tarefa)
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
 
+    setTimeout(textoTrf.value = "", 200)
 })
 
-// localStorage.clear()
+//Evento para limpar todas as tarefas
+botaoLpTrf.addEventListener('click', () => {
+    localStorage.clear()
+    listaDeTarefa.innerHTML = null
+})
+
+//Função para limpar uma tarefa em específico
+function excluirTarefa(Lixeira) {
+
+    const itemLista = Lixeira.parentElement
+
+    const nome = itemLista.getAttribute('identificador')
+
+    for (let i in tarefas) {
+        if(nome == tarefas[i]){
+            tarefas.splice(i, 1)
+        }
+    }
+
+    Lixeira.parentElement.remove()
+
+    ListaAtz = JSON.stringify(tarefas)
+
+    localStorage.setItem("tarefas", ListaAtz)
+
+}
+
+
 
 
